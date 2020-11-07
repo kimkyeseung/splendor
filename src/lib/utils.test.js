3b7ffdc7978135b49c6ee0c34640f21d5e583968
김계승 <kimkyeseung@gmail.com>
2020-11-08 04:11:18 +0900

    개발카드 구매 로직 테스트 작성: 황금 카드 사용시에 오류 발생하여 수정예정

--- src/lib/utils.test.js
+++ src/lib/utils.test.js
@@ -2,7 +2,7 @@ import DEVELOPMENT_CARDS from '../assets/developmentCards.json'
 import NOBLES from '../assets/nobles.json'
 
 import {
-  emptyHand, getDevelopmentValues
+  emptyHand, getDevelopmentValues, payDevelopmentPrice
 } from './utils'
 
 describe('개별 기능 작동 검사', () => {
@@ -10,8 +10,13 @@ describe('개별 기능 작동 검사', () => {
   const initialValues = {
     red: 0, green: 0, blue: 0, white: 0, black: 0
   }
+  const initialTokenAssets = {
+    red: 0, green: 0, blue: 0,
+    white: 0, black: 0, yellow: 0
+  }
   const currentPlayer = '1'
   const ctx = { currentPlayer }
+
   G.fields = {
     [currentPlayer]: {}
   }
@@ -26,16 +31,16 @@ describe('개별 기능 작동 검사', () => {
   })
 
   describe('emptyHand', () => {
-    G.fields[currentPlayer].hand = {
-      tokens: [],
-      development: null
-    }
     G.tokenStore = {
-      green: 0, white: 0, black: 0,
-      blue: 0, red: 0, yellow: 5
+      ...initialTokenAssets,
+      yellow: 5
     }
 
     it('손에 가지고 있는 토큰이 없어야 한다.', () => {
+      G.fields[currentPlayer].hand = {
+        tokens: [],
+        development: null
+      }
       G.fields[currentPlayer].hand.tokens.push('white')
       G.fields[currentPlayer].hand.tokens.push('green')
       G.fields[currentPlayer].hand.tokens.push('blue')
@@ -45,10 +50,215 @@ describe('개별 기능 작동 검사', () => {
     })
 
     it('손에 가지고 있는 개발카드가 없어야 한다.', () => {
-      G.fields[currentPlayer].hand.development = { name: "127R" }
+      G.fields[currentPlayer].hand.development = { name: '127R' }
       emptyHand(G, ctx)
 
       expect(G.fields[currentPlayer].hand.development).toBeNull()
+    })
+  })
+
+  describe('payDevelopmentPrice', () => {
+    G.fields[currentPlayer].hand = {
+      development: null
+    }
+
+    it('토큰만으로 단일 비용 개발 카드 구매', () => {
+      const target = {
+        grade: 1,
+        id: '110G',
+        value: 'green',
+        valueAmount: 1,
+        victoryPoint: 1,
+        cost: {
+          black: 4
+        },
+        set: 'original'
+      }
+
+      G.tokenStore = { ...initialTokenAssets }
+      G.fields[currentPlayer].developments = []
+      G.fields[currentPlayer].tokenAssets = {
+        ...initialTokenAssets,
+        black: 4
+      }
+      G.fields[currentPlayer].hand = {
+        development: {
+          name: target.id
+        }
+      }
+
+      payDevelopmentPrice(G, ctx)
+
+      expect(G.tokenStore).toEqual({
+        ...initialTokenAssets,
+        black: 4
+      })
+      expect(G.fields[currentPlayer].tokenAssets).toEqual(initialTokenAssets)
+    })
+
+    it('개발 카드로 비용을 할인받아 단일 비용 개발 카드 구매', () => {
+      const target = {
+        grade: 1,
+        id: '110G',
+        value: 'green',
+        valueAmount: 1,
+        victoryPoint: 1,
+        cost: {
+          black: 4
+        },
+        set: 'original'
+      }
+
+      G.tokenStore = { ...initialTokenAssets }
+      G.fields[currentPlayer].developments = [
+        Object.keys(DEVELOPMENT_CARDS).find(dev => DEVELOPMENT_CARDS[dev].value === 'black')
+      ]
+      G.fields[currentPlayer].tokenAssets = {
+        ...initialTokenAssets,
+        black: 3
+      }
+      G.fields[currentPlayer].hand = {
+        development: {
+          name: target.id
+        }
+      }
+
+      payDevelopmentPrice(G, ctx)
+
+      expect(G.tokenStore).toEqual({
+        ...initialTokenAssets,
+        black: 3
+      })
+      expect(G.fields[currentPlayer].tokenAssets).toEqual(initialTokenAssets)
+    })
+
+    it('토큰만으로 복합 비용 개발 카드 구매', () => {
+      const target = {
+        grade: 3,
+        id: '312K',
+        value: 'black',
+        valueAmount: 1,
+        victoryPoint: 3,
+        cost: {
+          red: 3,
+          green: 5,
+          blue: 3,
+          white: 3
+        },
+        set: 'original'
+      }
+
+      G.tokenStore = { ...initialTokenAssets }
+      G.fields[currentPlayer].developments = []
+      G.fields[currentPlayer].tokenAssets = {
+        ...initialTokenAssets,
+        red: 5,
+        green: 5,
+        blue: 5,
+        white: 5
+      }
+      G.fields[currentPlayer].hand = {
+        development: {
+          name: target.id
+        }
+      }
+
+      payDevelopmentPrice(G, ctx)
+
+      expect(G.tokenStore).toEqual({
+        ...initialTokenAssets,
+        red: 3,
+        green: 5,
+        blue: 3,
+        white: 3
+      })
+      expect(G.fields[currentPlayer].tokenAssets).toEqual({
+        ...initialTokenAssets,
+        red: 2, blue: 2, white: 2
+      })
+    })
+
+    it('개발 카드로 비용을 할인받아 복합 비용 개발 카드 구매', () => {
+      const target = {
+        grade: 3,
+        id: '312K',
+        value: 'black',
+        valueAmount: 1,
+        victoryPoint: 3,
+        cost: {
+          red: 3,
+          green: 5,
+          blue: 3,
+          white: 3
+        },
+        set: 'original'
+      }
+
+      G.tokenStore = { ...initialTokenAssets }
+      G.fields[currentPlayer].developments = [
+        Object.keys(DEVELOPMENT_CARDS).find(dev => DEVELOPMENT_CARDS[dev].value === 'red'),
+        Object.keys(DEVELOPMENT_CARDS).find(dev => DEVELOPMENT_CARDS[dev].value === 'green'),
+        Object.keys(DEVELOPMENT_CARDS).find(dev => DEVELOPMENT_CARDS[dev].value === 'blue')
+      ]
+      G.fields[currentPlayer].tokenAssets = {
+        ...initialTokenAssets,
+        red: 2,
+        green: 4,
+        blue: 2,
+        white: 3
+      }
+      G.fields[currentPlayer].hand = {
+        development: {
+          name: target.id
+        }
+      }
+
+      payDevelopmentPrice(G, ctx)
+
+      expect(G.tokenStore).toEqual({
+        ...initialTokenAssets,
+        red: 2,
+        green: 4,
+        blue: 2,
+        white: 3
+      })
+      expect(G.fields[currentPlayer].tokenAssets).toEqual({ ...initialTokenAssets })
+    })
+
+    it('토큰이 부족하지만 황금토큰 사용으로 단일 비용 개발 카드 구매', () => {
+      const target = {
+        grade: 1,
+        id: '110G',
+        value: 'green',
+        valueAmount: 1,
+        victoryPoint: 1,
+        cost: {
+          black: 4
+        },
+        set: 'original'
+      }
+
+      G.tokenStore = { ...initialTokenAssets }
+      G.fields[currentPlayer].developments = []
+      G.fields[currentPlayer].tokenAssets = {
+        ...initialTokenAssets,
+        black: 3,
+        yellow: 1
+      }
+      G.fields[currentPlayer].hand = {
+        development: {
+          name: target.id
+        }
+      }
+
+      payDevelopmentPrice(G, ctx)
+
+      expect(G.tokenStore).toEqual({
+        ...initialTokenAssets,
+        black: 3,
+        yellow: 1
+      })
+      expect(G.fields[currentPlayer].tokenAssets).toEqual(initialTokenAssets)
     })
   })
 })
