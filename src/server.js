6b4f211650672916062d71566970256d43b59b5f
김계승 <kimkyeseung@gmail.com>
2020-11-10 20:56:10 +0900

    heroku 에서 작동할 server.js 파일 위치 조정

--- src/server.js
+++ src/server.js
@@ -1,20 +1,6 @@
 import { Server } from 'boardgame.io/server'
-import path from 'path'
-import serve from 'koa-static'
 import game from './lib/game'
 
 const server = Server({ games: [game(4)] })
-const PORT = process.env.PORT || 8000
 
-// Build path relative to the server.js file
-const frontEndAppBuildPath = path.resolve(__dirname, './build')
-server.app.use(serve(frontEndAppBuildPath))
-
-server.run(PORT, () => {
-  server.app.use(
-    async (ctx, next) => await serve(frontEndAppBuildPath)(
-      Object.assign(ctx, { path: 'index.html' }),
-      next
-    )
-  )
-})
+server.run(8000)
